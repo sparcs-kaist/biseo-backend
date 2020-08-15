8b338e8fe7f472d6118a788cad5a9184b761ed52
pacokwon <haechank@gmail.com>
2020-08-15 20:54:40 +0900

    add: reformat code according to editorconfig

--- socket.js
+++ socket.js
@@ -1,35 +1,48 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-const username = ["Jack", "Lukas", "James", "Oliver", "Sophia", "Emma", "Aria", "Amelia"] // SSO 로그인 구현 전 임시 유저 배열
-let accessors = [] // 접속자 배열
+const username = [
+    "Jack",
+    "Lukas",
+    "James",
+    "Oliver",
+    "Sophia",
+    "Emma",
+    "Aria",
+    "Amelia",
+]; // SSO 로그인 구현 전 임시 유저 배열
+let accessors = []; // 접속자 배열
 
-module.exports = io => {
-    io.on('connection', socket => {
-        let decoded = ''
+module.exports = (io) => {
+    io.on("connection", (socket) => {
+        let decoded = "";
         try {
-            decoded = jwt.verify(socket.handshake.query['token'], process.env.JWT_SECRET)
+            decoded = jwt.verify(
+                socket.handshake.query["token"],
+                process.env.JWT_SECRET
+            );
         } catch (err) {}
-        const user = decoded.sparcs_id || username[Math.floor(Math.random() * username.length)]  // 랜덤 유저 이름 추출 (역시나 임시적)
+        const user =
+            decoded.sparcs_id ||
+            username[Math.floor(Math.random() * username.length)]; // 랜덤 유저 이름 추출 (역시나 임시적)
 
         if (!accessors.includes(user)) {
-            accessors.push(user)
-            socket.broadcast.emit('enter', user) // 전체 유저에게 누가 들어왔는지 보내줌
+            accessors.push(user);
+            socket.broadcast.emit("enter", user); // 전체 유저에게 누가 들어왔는지 보내줌
         }
 
-        socket.emit('members', accessors)
-        io.to(socket.id).emit('name', user) // 접속한 유저에게 랜덤으로 추출된 이름을 보내줌
+        socket.emit("members", accessors);
+        io.to(socket.id).emit("name", user); // 접속한 유저에게 랜덤으로 추출된 이름을 보내줌
 
-        socket.on('disconnect', () => {
-            let idx = accessors.indexOf(user)
-            if (idx > -1) accessors.splice(idx, 1) // disconnect된 유저이름의 index 찾아서 접속자 배열에서 지움
+        socket.on("disconnect", () => {
+            let idx = accessors.indexOf(user);
+            if (idx > -1) accessors.splice(idx, 1); // disconnect된 유저이름의 index 찾아서 접속자 배열에서 지움
 
-            socket.broadcast.emit('members', accessors) // 접속자가 변경되었으므로 전체 유저에게 변경된 접속자를 보내줌
-            socket.broadcast.emit('out', user) // 전체 유저에게 누가 나갔는지 보내줌
-        })
+            socket.broadcast.emit("members", accessors); // 접속자가 변경되었으므로 전체 유저에게 변경된 접속자를 보내줌
+            socket.broadcast.emit("out", user); // 전체 유저에게 누가 나갔는지 보내줌
+        });
 
-        socket.on('chat message', message => {
-            socket.broadcast.emit('chat message', user, message) // 유저가 chat message 로 메시지를 socket에게 보냄 -> 전체에게 메시지 뿌려줌
-        })
+        socket.on("chat message", (message) => {
+            socket.broadcast.emit("chat message", user, message); // 유저가 chat message 로 메시지를 socket에게 보냄 -> 전체에게 메시지 뿌려줌
+        });
     });
 };
-
