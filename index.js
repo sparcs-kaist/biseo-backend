704a8638262143a3454f7774bec7bd2d466bc0f0
kreeos <kanghyunkrislee@gmail.com>
2020-10-10 07:54:59 +0000

    Unknown Error Resolved

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const socketEvents = require('./socket.js')
 
 const server = http.createServer(app)
 
-server.listen(app.get('port'), ()=>{
-  console.log('sso server is running on 3000')
-})
 const socketServer = socketApp.listen(3002, ()=>{
-  console.log('socket server is running on 3002')
+  console.log(`server is running on 3002`)
 })
 
 socketApp.get('/', (req,res)=>{
