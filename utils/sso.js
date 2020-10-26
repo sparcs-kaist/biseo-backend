2942ebdb57cdf2f7aafc8ca18322ec67233ebcbc
pacokwon <haechank@gmail.com>
2020-10-26 13:34:46 +0000

    update: initialize dotenv at index

--- utils/sso.js
+++ utils/sso.js
@@ -1,8 +1,5 @@
 const Client = require('./sparcsssov2-node.js')
 
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../config/.env') })
-
 const client = new Client (process.env.SSO_CLIENT_ID, process.env.SSO_SECRET, false) // TODO: MOVE TO DOTENV
 
 module.exports = client
