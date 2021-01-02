1c76cb4e6fdc771ec3cc307e296d66ba4654f1bb
pacokwon <haechank@gmail.com>
2021-01-02 13:59:39 +0000

    bugfix: use process.env.SSO_CLIENT_SECRET instead of process.env.SSO_SECRET

--- utils/sso.js
+++ utils/sso.js
@@ -1,7 +1,8 @@
 import Client from './sparcsssov2-node';
+
 const client = new Client(
     process.env.SSO_CLIENT_ID,
-    process.env.SSO_SECRET,
+    process.env.SSO_CLIENT_SECRET,
     false
 );
 
