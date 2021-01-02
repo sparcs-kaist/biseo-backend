f809c3dbe7fbe4d45a47ba6d6d425388d8317c33
pacokwon <haechank@gmail.com>
2021-01-03 02:58:58 +0900

    feat: additional user vote status in /api/votes
    /api/votes now returns 1) whether or not a user has voted to an item
    or not in `hasAlreadyVoted`, and 2) which option the user has chosen
    if `hasAlreadyVoted` is true. null otherwise

--- routes/votes.js
+++ routes/votes.js
@@ -5,9 +5,51 @@ const router = express.Router();
 
 // GET /api/votes
 router.get('/', async (req, res) => {
-    const votes = await Vote.find({})
-        .sort({ expires: -1 })
-        .catch(err => console.error(err));
+    const { sparcs_id } = req.decoded;
+
+    const votes = await Vote.aggregate([
+        {
+            $sort: {
+                expires: -1
+            }
+        },
+        {
+            $addFields: {
+                userVoteStatus: {
+                    $reduce: {
+                        input: '$submissions',
+                        initialValue: { hasAlreadyVoted: false, choice: null },
+                        in: {
+                            $cond: {
+                                if: {
+                                    $or: [
+                                        '$$value.hasAlreadyVoted',
+                                        { $ne: ['$$this.username', sparcs_id] }
+                                    ]
+                                },
+                                then: '$$value',
+                                else: {
+                                    hasAlreadyVoted: true,
+                                    choice: '$$this.choice'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                hasAlreadyVoted: '$userVoteStatus.hasAlreadyVoted',
+                userChoice: '$userVoteStatus.choice',
+                choices: 1,
+                title: 1,
+                content: 1,
+                subtitle: 1,
+                expires: 1
+            }
+        }
+    ]).catch(err => console.error(err));
 
     res.json({ votes });
 });
