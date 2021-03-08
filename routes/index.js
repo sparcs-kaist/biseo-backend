bc56c0ede997dd75469f8581abdec912d79309c0
pacokwon <haechank@gmail.com>
2021-03-08 23:00:28 +0900

    refactor(votes): remove votes router completely

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import * as auth from '../controllers/auth.js';
-import votesRouter from './votes';
 import agendasRouter from './agendas';
 import { authMiddleware } from '../middlewares';
 
@@ -9,7 +8,6 @@ const router = express.Router();
 router.get('/auth/check', auth.authCheck);
 router.post('/login', auth.login);
 router.get('/login/callback', auth.loginCallback);
-router.use('/votes', authMiddleware, votesRouter);
 router.use('/agendas', authMiddleware, agendasRouter);
 
 export default router;
