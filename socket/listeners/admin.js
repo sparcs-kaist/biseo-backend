cf9f8d32ac4074b01ace5753b42a91d843e423e5
pacokwon <haechank@gmail.com>
2021-03-08 22:56:54 +0900

    feat(admin-listener): reword terminology and agenda creation logic

--- socket/listeners/admin.js
+++ socket/listeners/admin.js
@@ -1,42 +1,45 @@
-import Vote from '../../models/vote';
+import Agenda from '../../models/agenda';
 
 /*
  * adminListener - register 'admin:create' event to socket
  *   the 'admin:create' event is sent to the server socket when an administrator
- *   creates a new vote item. the admin will send a payload, which is an object
- *   that has 4 members: title, content, subtitle, choices. refer to the Vote schema
+ *   creates a new agenda item. the admin will send a payload, which is an object
+ *   that has 4 members: title, content, subtitle, choices. refer to the Agenda schema
  *   for further information on these members.
  *
- *   this event listener creates a new vote and broadcasts the created vote to all
+ *   this event listener creates a new agenda and broadcasts the created agenda to all
  *   client sockets.
  */
 export const adminListener = (io, socket) => {
     socket.on('admin:create', async (payload, callback) => {
-        // payload has 4 members. title, content, subtitle, choices
+        // payload has 4 fields. title, content, subtitle, choices
         const currentTime = Date.now();
-
-        // vote lasts for 3 minutes. this value is arbitrary and temporary
+        // agenda lasts for 3 minutes. this value is arbitrary and temporary
         const validDuration = 3 * 60 * 1000;
 
-        const newVoteItem = new Vote({
+        // all choices are initialized with a vote count of 0
+        const votesCountMap = new Map(
+            payload.choices.map(choice => [choice, 0])
+        );
+
+        const newAgenda = new Agenda({
             ...payload,
-            submissions: [],
+            votesCountMap,
             expires: new Date(currentTime + validDuration)
         });
 
-        const result = await newVoteItem.save().catch(() => {
+        const result = await newAgenda.save().catch(error => {
             console.error('Error while inserting new vote');
-            callback({ success: false });
+            callback({ success: false, message: error.message });
         });
 
         if (!result) {
             callback({ success: false });
             return;
         }
-        callback({ success: true });
 
-        const { _id, title, content, subtitle, choices, expires } = result;
-        io.emit('vote:created', {
+        const { _id, title, content, subtitle, expires, choices } = result;
+        io.emit('agenda:created', {
             _id,
             title,
             content,
@@ -44,5 +47,6 @@ export const adminListener = (io, socket) => {
             choices,
             expires
         });
+        callback({ success: true });
     });
 };
