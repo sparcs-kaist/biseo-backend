d6f954f4ad72c991511dcc9a5d59f9660b480f03
pacokwon <haechank@gmail.com>
2021-03-08 22:54:52 +0900

    feat(agenda): define agenda router to replace /api/votes

---
+++ routes/agendas.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import Agenda from '../models/agenda';
+import Vote from '../models/vote';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const now = new Date();
+    const agendas = await Agenda.find({
+        expires: { $gt: now }
+    })
+        .limit(10)
+        .sort({ expires: -1 })
+        .lean();
+
+    const agendaIds = agendas.map(({ _id }) => _id);
+
+    const votes = await Vote.find({
+        agenda: { $in: agendaIds },
+        username: req.decoded.sparcs_id
+    });
+
+    const agendasResponse = agendas.map(agenda => {
+        const userVote = votes.find(vote => vote.agenda.equals(agenda._id));
+
+        return {
+            ...agenda,
+            userChoice: userVote?.choice ?? null
+        };
+    });
+
+    res.json({ agendas: agendasResponse });
+});
+
+router.get('/:id', async (req, res) => {
+    const { sparcs_id } = req.decoded;
+    const _id = req.params.id;
+
+    const agendas = await Agenda.findOne({ _id });
+    const userVote = await Vote.findOne({ agenda: _id, username: sparcs_id });
+
+    res.json({
+        ...agendas,
+        userChoice: userVote?.choice ?? null
+    });
+});
+
+export default router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as auth from '../controllers/auth.js';
 import votesRouter from './votes';
+import agendasRouter from './agendas';
 import { authMiddleware } from '../middlewares';
 
 const router = express.Router();
@@ -9,5 +10,6 @@ router.get('/auth/check', auth.authCheck);
 router.post('/login', auth.login);
 router.get('/login/callback', auth.loginCallback);
 router.use('/votes', authMiddleware, votesRouter);
+router.use('/agendas', authMiddleware, agendasRouter);
 
 export default router;
