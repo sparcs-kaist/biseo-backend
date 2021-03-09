3709442a0e56e930b99f28a9bfdf9c45a78f0fdf
pacokwon <haechank@gmail.com>
2021-03-10 01:25:17 +0900

    feat(sso): throw error on empty sso credentials

--- src/utils/sso.ts
+++ src/utils/sso.ts
@@ -1,9 +1,10 @@
 import Client from './sparcsssov2-node';
 
-const client = new Client(
-  process.env.SSO_CLIENT_ID,
-  process.env.SSO_CLIENT_SECRET,
-  false
-);
+const { SSO_CLIENT_ID, SSO_CLIENT_SECRET } = process.env;
+
+if (SSO_CLIENT_ID === undefined || SSO_CLIENT_SECRET === undefined)
+  throw new Error('SSO credentials are empty. Initialize your .env file!');
+
+const client = new Client(SSO_CLIENT_ID, SSO_CLIENT_SECRET, false);
 
 export default client;
