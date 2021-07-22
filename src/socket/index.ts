6783f0555a1f31f0b62ca2d6b70c53a97b83d58d
pacokwon <haechank@gmail.com>
2021-07-22 22:11:49 +0900

    refactor(socket): flatten main logic with async listener

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -17,30 +17,20 @@ export default (httpServer: http.Server): void => {
   io.use(authMiddleware);
 
   // main logic of listener socket
-  io.on('connection', socket => {
+  io.on('connection', async socket => {
+    const redisClient = redis.getConnection();
     const { sparcs_id, isAdmin } = socket.user;
 
-    const redisClient = redis.getConnection();
-    (async () => {
-      try {
-        const ctUser = await redisClient.hget('accessors', sparcs_id);
-        if (ctUser === null || ctUser === '0') {
-          redisClient.hset('accessors', sparcs_id, '1');
-          redisClient.hset('memberStates', sparcs_id, MemberState.ONLINE);
-          socket.broadcast.emit('chat:enter', sparcs_id); // broadcast the user's entrance
-        } else {
-          redisClient.hset(
-            'accessors',
-            sparcs_id,
-            String(parseInt(ctUser) + 1)
-          );
-        }
-        const members = await getConnectedMembers();
-        socket.emit('chat:members', members); // send list of members to new user
-      } catch (error) {
-        console.error(error);
-      }
-    })();
+    const accessors = await redisClient.hget('accessors', sparcs_id);
+    const accessCount = accessors !== null ? parseInt(accessors) : 0;
+    if (accessCount === 0) {
+      redisClient.hset('memberStates', sparcs_id, MemberState.ONLINE);
+      socket.broadcast.emit('chat:enter', sparcs_id); // broadcast the user's entrance
+    }
+    redisClient.hset('accessors', sparcs_id, accessCount + 1);
+
+    const members = await getConnectedMembers();
+    socket.emit('chat:members', members); // send list of members to new user
 
     socket.emit('chat:name', sparcs_id); // send sparcs_id to new user
 
