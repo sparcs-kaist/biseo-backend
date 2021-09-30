1903628f04daed2693705a2726a6e3a3192adc86
KyusungSeo <seo3650@kaist.ac.kr>
2021-09-30 22:15:23 +0900

    feat(vote): modified to allow only registered users to vote

--- src/socket/listeners/vote.ts
+++ src/socket/listeners/vote.ts
@@ -33,6 +33,9 @@ export const voteListener = (io: Server, socket: Socket): void => {
         )
           throw new Error(`Invalid Choice: ${choice} is not a votable choice`);
 
+        if (!agenda.participants.includes(username))
+          throw new Error('You are not registered user for this vote.');
+
         // increment votesCountMap count
         agenda.votesCountMap.set(
           choice,
