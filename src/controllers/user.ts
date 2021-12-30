1a62a911f398e1127d70fb48e8481d984badb8b4
KyusungSeo <seo3650@kaist.ac.kr>
2021-12-30 21:28:24 +0900

    feat(voter): add patch method of users

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -22,3 +22,51 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
   });
   res.json({ users: users });
 };
+
+export const updateUsers = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const preset: number = parseInt(req.query['preset'] as string) - 1;
+  if (isNaN(preset) || preset < 0 || preset > 2) {
+    res.status(401);
+    res.send('Preset is invalid value.');
+    return;
+  }
+
+  type User = { sparcsId: string; isVotable: boolean };
+  const users: User[] = req.body['users'];
+  if (users === undefined) {
+    res.status(401);
+    res.send('Request body is invalid.');
+    return;
+  }
+
+  users.forEach(async user => {
+    const sparcsId = user.sparcsId;
+    const isVotable = user.isVotable;
+
+    switch (preset) {
+      case 0:
+        await User.updateOne(
+          { sparcsId: sparcsId },
+          { $set: { 'isVotable.0': isVotable } }
+        );
+        break;
+      case 1:
+        await User.updateOne(
+          { sparcsId: sparcsId },
+          { $set: { 'isVotable.1': isVotable } }
+        );
+        break;
+      case 2:
+        await User.updateOne(
+          { sparcsId: sparcsId },
+          { $set: { 'isVotable.2': isVotable } }
+        );
+        break;
+    }
+    res.status(200);
+    res.end('success');
+  });
+};
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,5 +4,6 @@ import * as user from '@/controllers/user';
 const router = express.Router();
 
 router.get('/', user.getUsers);
+router.patch('/', user.updateUsers);
 
 export default router;
