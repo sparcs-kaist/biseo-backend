0f4ef63850b8c44c30bf1c9d569ccb72cc7306e3
KyusungSeo <seo3650@kaist.ac.kr>
2022-01-15 17:29:35 +0900

    fix(cors): change aloowed methods

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -4,7 +4,13 @@ import { RequestHandler } from 'express';
 export const corsMiddleware: RequestHandler = (req, res, next) => {
   res.set('Access-Control-Allow-Origin', req.get('origin'));
   res.set('Access-Control-Allow-Credentials', 'true');
-  res.set('Access-Control-Allow-Methods', '*');
+  res.set('Access-Control-Allow-Methods', [
+    'POST',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'DELETE',
+  ]);
   res.set('Access-Control-Allow-Headers', 'x-access-token');
 
   if (req.method === 'OPTIONS') res.sendStatus(200);
