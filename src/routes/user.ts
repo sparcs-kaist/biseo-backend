9022e65c4e859fe189613222b2518962e26e00a6
prectal123 <prectal123@naver.com>
2022-09-21 23:03:46 +0900

    feat: add name change

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', user.getUsers);
 router.patch('/', user.updateUsers);
+router.patch('/changename', user.changeName);
 router.delete('/', user.deleteUsers);
 router.post('/', user.addUser);
 
